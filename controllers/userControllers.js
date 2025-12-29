eff76c557780c6532de3c234b1148bc4af7c3c38
krishu2814 <krishukumarsingh06@gmail.com>
2025-12-29 22:44:32 +0530

    Get user implementation

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,30 @@
-// get user info
-const getUserController = async (req, res) => {};
+import User from "../models/userModel.js";
+
+const getUserController = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "User found successfully",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Error fetching user details",
+    });
+  }
+};
 
 export default getUserController;
